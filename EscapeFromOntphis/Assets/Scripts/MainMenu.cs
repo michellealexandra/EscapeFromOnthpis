63903109c4e259588e61ab61dd97125ff12ae1eb
Tinara Nathania <50069062+tinaranw@users.noreply.github.com>
2021-01-03 21:17:53 +0700

    Fix OnClick to GameScene

--- EscapeFromOntphis/Assets/Scripts/MainMenu.cs
+++ EscapeFromOntphis/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);

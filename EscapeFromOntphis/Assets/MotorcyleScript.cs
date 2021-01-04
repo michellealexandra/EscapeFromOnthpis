04d82ecd2821cacccd5dc11eac09e572ba1e6222
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-04 21:30:06 +0700

    yey motor jadi

---
+++ EscapeFromOntphis/Assets/MotorcyleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorcyleScript : MonoBehaviour
+{
+
+    public float motorSpeed;
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.y += Input.GetAxis ("Vertical") * motorSpeed * Time.deltaTime;
+
+        transform.position = position;
+    }
+}

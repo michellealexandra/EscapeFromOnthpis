1620f44c1476b4c97996b0176509699b6739dd40
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-05 00:36:34 +0700

    gem okay, tapi nubruk destroyer

---
+++ EscapeFromOntphis/Assets/Scripts/GemScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : MonoBehaviour
+{
+
+    public int scoreValue;
+
+    private void Start() {
+        scoreValue = 0;
+    }
+
+    private void Update() {
+        //update text score
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.tag == "gem"){
+        Debug.Log("Gem!!!!!!!!");
+        scoreValue += 10;
+        // Destroy(gameObject);
+        }
+    }
+}

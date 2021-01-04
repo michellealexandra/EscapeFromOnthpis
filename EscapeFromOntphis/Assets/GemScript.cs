ce9eb540df0d0f48a1b23a7ecebfa87110dd8784
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-04 22:45:10 +0700

    idk

---
+++ EscapeFromOntphis/Assets/GemScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemScript : MonoBehaviour
+{
+    
+    public GameObject prefabGem;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("hit detected");
+
+            if(collision.tag == "gem"){
+
+                var plus = Instantiate(prefabGem);
+
+                Destroy(this.gameObject);
+            }
+
+    }
+
+}

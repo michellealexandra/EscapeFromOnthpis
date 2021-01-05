2cbb9dd8eca9ecb72516fddc70245a335285092e
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-05 17:32:53 +0700

    fail, gem tidak ter destroy

--- EscapeFromOntphis/Assets/Scripts/GemScript.cs
+++ EscapeFromOntphis/Assets/Scripts/GemScript.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class GemScript : MonoBehaviour
 {
-
-    public int scoreValue;
+    public ScoreScript score;
 
     private void Start() {
-        scoreValue = 0;
+        // scoreValue = 0;
     }
 
     private void Update() {
         //update text score
     }
 
-    private void OnTriggerEnter2D(Collider2D collider) {
-        if(collider.gameObject.tag == "gem"){
-        Debug.Log("Gem!!!!!!!!");
-        scoreValue += 10;
-        // Destroy(gameObject);
+    // private void OnTriggerEnter2D(Collider2D collider) {
+    //     if(collider.gameObject.tag == "gem"){
+    //     Debug.Log("Gem!!!!!!!!");
+
+
+    //     Destroy(this.gameObject);
+    //     Destroy(collider.gameObject);
+
+    //     score.AddScore(10);
+    //     }
+    // }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.collider.tag == "gem"){
+            Debug.Log("Gem!!!!!!!!");
+
+            Destroy(this.gameObject);
+            Destroy(collision.collider.gameObject);
+
+            score.AddScore(10);
         }
     }
 }
---
+++ EscapeFromOntphis/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    int score = 0;
+    int gemCollected = 0;
+    public GameObject gem;
+    public Text txtScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    public void AddScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+
+        //menampilkan di UI
+        txtScore.text = score + "";
+    }
+}

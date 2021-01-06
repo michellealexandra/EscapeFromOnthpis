f016af5e111774a080b91b761e1f92b773c4ba1d
shrall <shrallvierdo@gmail.com>
2021-01-07 06:31:04 +0700

    background scroll fixed

--- EscapeFromOntphis/Assets/Scripts/ScrollScript.cs
+++ EscapeFromOntphis/Assets/Scripts/ScrollScript.cs
@@ -16,7 +16,7 @@ public class ScrollScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float newPos = Mathf.Repeat(Time.time*scrollSpeed, 17);
+        float newPos = Mathf.Repeat(Time.time*scrollSpeed, 17.90f);
         transform.position = startPos + Vector2.right* newPos;
     }
 }

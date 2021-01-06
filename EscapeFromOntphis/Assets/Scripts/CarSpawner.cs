80ce1f7369f2999bf0fa55fe646fa0812d7f654d
Tinara Nathania <50069062+tinaranw@users.noreply.github.com>
2021-01-06 19:31:30 +0700

    Spawn different type of cars done

--- EscapeFromOntphis/Assets/Scripts/CarSpawner.cs
+++ EscapeFromOntphis/Assets/Scripts/CarSpawner.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    public GameObject car;
+    public GameObject[] cars;
+    int carNo;
     public float maxPos = 0.9f;
     public float delayTimer = 1f;
     float timer;
@@ -20,7 +21,8 @@ public class CarSpawner : MonoBehaviour
         timer -= Time.deltaTime;
         if(timer <= 0){
             Vector3 carPos = new Vector3(transform.position.x, Random.Range(-2.5f, 0.9f), transform.position.z);
-            Instantiate(car, carPos, transform.rotation);
+            carNo = Random.Range (0,4);
+            Instantiate(cars[carNo], carPos, transform.rotation);
             timer = delayTimer;
         }
         

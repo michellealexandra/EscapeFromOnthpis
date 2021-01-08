66537651ef7d5fae4ae37cdaca1c6bdee887ccd2
Tinara Nathania <50069062+tinaranw@users.noreply.github.com>
2021-01-08 20:48:32 +0700

    Added bgm music on startmenu.game

---
+++ EscapeFromOntphis/Assets/Scripts/StartMusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMusicManager : MonoBehaviour
+{
+    private static StartMusicManager startMusicManagerInstance;
+    private bool playSound;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Update(){
+         if(SceneManager.GetActiveScene().buildIndex == 0 || SceneManager.GetActiveScene().buildIndex == 1){
+            playSound = true;
+        }else {
+            playSound = false;
+        }
+        checkSound();
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void checkSound(){
+        if(playSound == true){
+             startMusicManagerInstance = this;
+        } else {
+             Destroy(gameObject);
+        }
+    }
+}
